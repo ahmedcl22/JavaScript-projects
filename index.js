4c431e1326354edff08df9da2fa4c6593b5cc25f
Ahmed Suleiman <ahmedms200@gmail.com>
2025-02-05 22:26:29 +0300

    UPD tabs to JS page

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const tab1=document.getElementById("tab1");
+const tab2=document.getElementById("tab2");
+const tabBtn=document.getElementById("tabBtn");
+const box=document.getElementById("box");
+const photoContainer=document.getElementById("photo-container");
+tabBtn.onclick=function(){
+    if(tab2.checked){
+        box.style.display="flex"
+    }else if(tab1.checked){
+        box.style.display="none"
+    }
+}
+
+
+
+
+
+
+
+
 const overtext=document.getElementById("overtext");
 const userText=document.getElementById("userText");
 const submitBtn=document.getElementById("submitBtn");
